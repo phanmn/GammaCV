08fa904b26d0a6be2bc921e9c0e9f5930bb26f90
WorldThirteen <mihailzachepilo@gmail.com>
2019-06-23 19:03:13 +0300

    Move downsample operation kernel changes

--- lib/ops/downsample/kernel.glsl
+++ lib/ops/downsample/kernel.glsl
@@ -7,25 +7,33 @@
  */
 
 vec4 operation(float y, float x) {
-  vec4 value = vec4(0.0);
+  float r = 0.0;
+  float g = 0.0;
+  float b = 0.0;
   
   for (float dx = 0.0; dx < K; dx += 1.0) {
     for (float dy = 0.0; dy < K; dy += 1.0) {
-      vec4 v = pickValue_tSrc((y * K) + dy, (x * K) + dx);
+      vec4 v = pickValue_tSrc((y * K) - dy, (x * K) - dx);
 
       if (S == 0.0) {
-        value = v;
+        r = v.r;
+        g = v.g;
+        b = v.b;
       } 
 
       if (S == 1.0) {
-        value += v;
+        r += v.r;
+        g += v.g;
+        b += v.b;
       } 
     }
   }
 
   if (S == 1.0) {
-    value /= K * K;
+    r /= K * K;
+    g /= K * K;
+    b /= K * K;
   } 
 
-	return value;
+	return vec4(r, g, b, 1.0);
 }
